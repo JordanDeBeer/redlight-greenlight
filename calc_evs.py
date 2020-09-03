d235adb94eed8d6fd1a6aac444a237dc8f23caa3
Jordan DeBeer <jordan@jordandebeer.com>
2020-09-03 16:57:26 -0400

    Finalized algo

--- calc_evs.py
+++ calc_evs.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
+from typing import Dict, NamedTuple, Iterable, Iterator
+from dataclasses import dataclass
 from math import inf
 from itertools import product
 
-ev_map = {}
+number_red_cards = 12
+number_green_cards = 25
+number_dbl_green_cards = 14
+
+@dataclass(frozen=True)
+class BoardState:
+    score: tuple
+
+    spent_red: int 
+    spent_green: int
+    spent_double_green: int
+    
+    opponent_score: tuple
+
+
+
+ev_map: Dict[BoardState, int] = {}
 
 positions = [i for i in range(0,16)] # 17 is a win
 stacks = [i for i in range(0,16)]
@@ -10,19 +28,47 @@ stacks = [i for i in range(0,16)]
 possible_scores = [i for i in product(positions, stacks) if i[0]+i[1]<17]
 
 # Spent cards
-redCards = [i for i in range(0,12)]
-greenCards = [i for i in range(0,25)]
-dblGreenCards = [i for i in range(0,14)]
+red_cards = [i for i in range(0,12)]
+green_cards = [i for i in range(0,25)]
+dbl_green_cards = [i for i in range(0,14)]
 
-possible_turn_states = product(possible_scores, redCards, greenCards, dblGreenCards)
+# States where drawing is a guaranteed win
+infinite_states = [BoardState(i) for i in product(possible_scores, red_cards, green_cards, dbl_green_cards, possible_scores) if i[1]==12 and 1*i[2] +2*i[3] >= 17]
 
-infinite_states = [i for i in possible_turn_states if redCards==12 and 1*i[2] +2*i[3] >= 17]
-zero_states = [i for i in possible_turn_states if i[0][1]==0 and i[2]==25 and i[3]==14]
+# States where we are forced to draw a red card
+zero_states = [BoardState(i) for i in product(possible_scores, red_cards, green_cards, dbl_green_cards, possible_scores) if i[0][1]==0 and i[2]==25 and i[3]==14]
 
 # Constants for wins where there are 0 red cards left and drawing would result in a win
 for i in infinite_states:
-    ev_map[f"{i[0][0]}-{i[0][1]}-12-{i[2]}-{i[3]}"] = inf
+    ev_map[i] = inf
 
 # Constants for when there is only red cards left at the start of your turn.
-for position in zero_states:
-    ev_map[f"{i[0][0]}-0-0-25-14"] = 0
+for i in zero_states:
+    ev_map[i] = 0
+
+
+def FindEV():
+    for t in product(possible_scores, red_cards, green_cards, dbl_green_cards, possible_scores):
+        bs = BoardState(t[0],t[1],t[2],t[3],t[4])
+        if bs not in ev_map:
+            print("bs not in map: ", bs)
+            cards_left = 51-bs.spent_red+bs.spent_green+bs.spent_double_green
+            red_probability = number_red_cards-bs.spent_red / cards_left
+            green_probability = number_green_cards-bs.spent_green / cards_left
+            dbl_green_probability = number_dbl_green_cards-bs.spent_double_green / cards_left
+
+            r_bs = BoardState(bs.score,bs.spent_red+1,bs.spent_green,bs.spent_double_green,bs.opponent_score)
+            g_bs = BoardState(bs.score[1]+1,bs.spent_red,bs.spent_green+1,bs.spent_double_green,bs.opponent_score)
+            dg_bs = BoardState(bs.score[1]+2,bs.spent_red,bs.spent_green,bs.spent_double_green+1,bs.opponent_score)
+            
+            raw_ev = -bs.score[1]*red_probability + ev_map[g_bs]*green_probability + ev_map[dg_bs]*dbl_green_probability
+
+            # Calc value of removing card from oppt
+            actual_ev = raw_ev - (sum(ev_map[r_bs], ev_map[g_bs],ev_map[dg_bs])/3)
+
+            print("added bs to map ", bs)
+            ev_map[bs]= actual_ev
+        else:
+            print("bs in map ", bs)
+
+FindEV()
--- probabilities/probabilities.go
+++ probabilities/probabilities.go
@@ -6,6 +6,10 @@ import (
 	"github.com/JordanDeBeer/redlight-greenlight/game"
 )
 
+// EVMap is a map[string]float32 where the key is a string representation of a turn state to the estimated value of a turn
+// Format is ${stack}-${spendRed}-${spentGreen}-${spendDblGreen}
+var EVMap map[string]float32
+
 func countSpentCards(spentDeck []game.Card, cardColor game.Card) (count int) {
 	for _, card := range spentDeck {
 		if card == cardColor {
@@ -15,7 +19,14 @@ func countSpentCards(spentDeck []game.Card, cardColor game.Card) (count int) {
 	return count
 }
 
+func newEvMap()
+
 func FindEv(count, stack int, spentDeck []game.Card) (ev float32) {
+	// Seed some initial values to get started.
+	// Single red card left.
+	EVMap[fmt.Sprintf("%v-%v-%v-%v", 0,game.NumberRedCards,game.GreenCard,game.DblGreenCard)] = 0
+
+
 	cardsLeft := len(spentDeck)
 	spentRed := countSpentCards(spentDeck, game.RedCard)
 	spentGreen := countSpentCards(spentDeck, game.GreenCard)
@@ -46,6 +57,7 @@ func PredictDraw(position, stack int, spentRed, spentGreen, spentDblGreen int) b
 	dblGreenProb := float32(game.NumberDblGreenCards-spentDblGreen) / float32(cardsLeft)
 
 	expectedValue := float32(-stack)*redProb + 1*greenProb + 2*dblGreenProb
+	fmt.Println(expectedValue)
 
 	oppRedProb := float32(game.NumberRedCards-spentRed-1) / float32(cardsLeft-1)
 	oppGreenProb := float32(game.NumberGreenCards-spentGreen-1) / float32(cardsLeft-1)
